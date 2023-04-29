42d89a5b8f8a7309a2837412876a8cec953c34b5
tyy <465668878@qq.com>
2023-04-29 09:42:49 +0800

    zhushi

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LeetCode
 # LeetCode
+
+
+## 2023/04/29 9:33
---
+++ demo.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){  
+
+    cout<<"Hello world"<<endl;
+    system("pause>0");
+
+    return 0;
+}
